e848141073114f02e8dbd89384312c0083ecb2ad
aditya-Home <adityasekhar03@gmail.com>
2014-05-11 18:57:42 +0530

    Added Driver listener exception

--- Framework/src/main/java/org/seltest/driver/DriverManager.java
+++ Framework/src/main/java/org/seltest/driver/DriverManager.java
@@ -1,12 +1,17 @@
 package org.seltest.driver;
 
 import org.openqa.selenium.WebDriver;
+import org.seltest.core.SelTestException;
 
 public class DriverManager {
 	private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
 
 	public static WebDriver getDriver() {
-		return webDriver.get();
+		WebDriver driver = webDriver.get();
+		if(driver==null){
+			throw new SelTestException(" Missing driverListener.jar in project classpath");
+		}
+		return driver;
 	}
 
 	public static void setWebDriver(WebDriver driver) {
