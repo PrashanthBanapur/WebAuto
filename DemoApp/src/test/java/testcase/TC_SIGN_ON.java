728dcd76a8308988d120f9bea414246c665ae020
aditya-Home <adityasekhar03@gmail.com>
2014-05-11 20:48:50 +0530

    Added Sample Suite and report in Demo App

--- DemoApp/src/test/java/testcase/TC_SIGN_ON.java
+++ DemoApp/src/test/java/testcase/TC_SIGN_ON.java
@@ -1,9 +1,12 @@
 package testcase;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.seltest.driver.DriverListener;
 
 import pageobject.SignOnPage;
 
+@Listeners({DriverListener.class})
 public class TC_SIGN_ON extends Tests{
   @Test
   public void SignOn() {
